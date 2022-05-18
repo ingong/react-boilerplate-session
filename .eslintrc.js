08bd343288a278fe3be444a8898b0eb226333a86
ingong <insong1216@gmail.com>
2022-05-18 23:24:22 +0900

    [config]: babel/eslint-parser 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
         "eslint:recommended",
         "plugin:react/recommended"
     ],
+    "parser": "@babel/eslint-parser",
     "parserOptions": {
         "ecmaFeatures": {
             "jsx": true
